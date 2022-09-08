be9b0a12329a5b75d64d0c44b2d78e9bc3b9ae69
David Díez <david.diez@tokiota.com>
2022-09-08 13:43:34 +0200

    [client] fixed post endpoint

--- client/src/services/chat.service.ts
+++ client/src/services/chat.service.ts
@@ -2,10 +2,10 @@ import { MessageInterface } from '../shared/interfaces';
 import { BaseService } from './base.service';
 
 class ChatService extends BaseService {
-  endpoint = 'http://localhost:2023/';
+  #endpoint = 'http://localhost:2023/';
 
   sendMessage = (message: MessageInterface): Promise<void> =>
-    this.postData('chat', message);
+    this.postData(`${this.#endpoint}chat`, message);
 }
 
 export const chatService = new ChatService();
