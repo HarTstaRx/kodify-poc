491319588be6a6e7b76158d7d9298cc5eee8447b
David Díez <david.diez@tokiota.com>
2022-09-08 11:29:29 +0200

    [server] added cors

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,11 @@ import express from 'express';
 const router = express.Router();
 const app = express();
 
+app.use((_request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 router.post('/chat', (request, response) => {
   // TODO
   response.sendStatus(200);
